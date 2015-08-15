ce4099c23416f9af2cb26371ab42ce7eddb39c5f
Michael Dominick <michael@buccaneertech.com>
2015-08-14 23:58:43 -0400

    Initial commit + Rails 4 default DateTime compatibility

--- BTChronometer/BTChronometer.m
+++ BTChronometer/BTChronometer.m
@@ -3,11 +3,47 @@
 //  BTChronometer
 //
 //  Created by Michael Dominick on 8/14/15.
-//  Copyright (c) 2015 Buccaneer Tech. All rights reserved.
+//  Copyright (c) 2015 Buccaneer Tech.
 //
 
 #import "BTChronometer.h"
 
 @implementation BTChronometer
+
++ (BTChronometer *)sharedChronomater {
+    static dispatch_once_t once;
+    static BTChronometer* sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
++ (NSDate *)dateFromUnformattedRailsString:(NSString *)dateString {
+    return [[BTChronometer sharedChronomater] dateFromUnformattedRailsString:dateString];
+}
+
+- (NSDate *)dateFromUnformattedRailsString:(NSString *)dateString {
+    NSDate *date = nil; // we return a Nil value for an error
+    if ([self inputIsValid:dateString]) {
+        NSDateFormatter* dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSz"];
+        date = [dateFormat dateFromString:dateString];
+    }
+
+    return date;
+}
+
+#pragma mark Validations
+
+- (BOOL)inputIsValid:(NSString *)inputString {
+    BOOL isValid = YES;
+    if (!inputString || [inputString isKindOfClass:[NSNull class]]
+        || [inputString isEqualToString:@""]) {
+        isValid = NO;
+        NSLog(@"BTChronometer: Your date string input is invalid!");
+    }
+    return isValid;
+}
 
 @end
