28b66cafb4aae2341be8cc5ca0651babdd8ea90b
Michael Dominick <michael@buccaneertech.com>
2015-08-14 23:45:06 -0400

    Initial Commit

---
+++ BTChronometer/BTChronometer.h
@@ -0,0 +1,13 @@
+//
+//  BTChronometer.h
+//  BTChronometer
+//
+//  Created by Michael Dominick on 8/14/15.
+//  Copyright (c) 2015 Buccaneer Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTChronometer : NSObject
+
+@end
---
+++ BTChronometer/BTChronometer.m
@@ -0,0 +1,13 @@
+//
+//  BTChronometer.m
+//  BTChronometer
+//
+//  Created by Michael Dominick on 8/14/15.
+//  Copyright (c) 2015 Buccaneer Tech. All rights reserved.
+//
+
+#import "BTChronometer.h"
+
+@implementation BTChronometer
+
+@end
